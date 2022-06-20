3f6ddf7e7c87f0033df65b06237c2503bc3f496b
The-WindowsVista <67670160+The-WindowsVista@users.noreply.github.com>
2022-06-19 22:21:30 -0400

    add installer script

---
+++ installer.sh
@@ -0,0 +1,4 @@
+echo "Make sure this is ran as root, or enter the password when asked."
+echo "also, run in same directory as AURchr itself"
+sudo cp ./aurchr /bin/aurchr
+echo "Done! enjoy slightly easier AURing!"
